3b7f28c52bf4148264495dffacde8306d8b73756
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-16 08:44:47 -0700

    Passed recipe data to RecipeContainer. Used a map to loop through recipes and rendered each recipe with Recipe.jsx.

--- src/components/RecipeContainer.jsx
+++ src/components/RecipeContainer.jsx
@@ -1,9 +1,14 @@
-//Higher-Order Component
+// Importing child/presentational component to Higher-Order Component
+import Recipe from "./Recipe.jsx"
 
-function RecipeContainer() {
+//Higher-Order Component - Passing in information from App to be used as props by Recipe
+function RecipeContainer({ recipes }) {
     return(
         <div>
-
+            {/* Using a map to loop through the recipes and rendering a Recipe component for each */}
+            {/* Created a key since an id was not provided with the dataset */}
+            {recipes.map(( recipe, index ) => <Recipe key={`${index}${recipe.name}`} 
+            name={recipe.name} ingredients={recipe.ingredients}/>)}
         </div>
     )
 }
