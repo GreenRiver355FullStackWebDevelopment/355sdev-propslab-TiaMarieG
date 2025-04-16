b1255f8fa243f8ac1dc2a38029e0399c147149e3
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-16 10:49:01 -0700

    Deconstructed props and added them to Recipe.jsx to be rendered. Didn't like how ingredients and instructions were a giant wall of text so added a map to them so they would be iterated over.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,11 +1,26 @@
 // Presentational Component
-// Destructuring props to explicity call name and ingredients
-function Recipe({ recipe }) {
-    return(
-        <>
-        <h2>{recipe}</h2>
-        </>
-    )
+// Destructuring props to explicity call recipe, ingredients, and instructions
+function Recipe({ recipe, ingredients, instructions }) {
+  return (
+    <>
+      <h2>{recipe}</h2>
+      <h3>Ingredients</h3>
+      <ul>
+        {/* Adding a map to iterate over the ingredients object so that they can be placed on separate lines */}
+        {ingredients.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
+      <h3>Instructions</h3>
+      <ol>
+        {/* Adding a map to iterate over the instructions object so that they can be placed on separate lines */}
+        {instructions.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+    </>
+  );
 }
 
-export default Recipe
+export default Recipe;
