dbc106962f53c3a756923db046d8c02e372b7608
Tia Marie Gordon <tmgordon8192@gmail.com>
2025-04-17 11:17:36 -0700

    Installed MUI and added some basic styling. Ix helped me to create a state that allows a button to show and hide the recipe instructions.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,29 +1,42 @@
 // Importing the CSS
 import './Recipe.css'
+import {Button, Typography, Card, CardHeader, CardContent } from '@mui/material';
+// Help from Ix
+import { useState } from 'react';
 
 // Presentational Component
 // Destructuring props to explicity call recipe, ingredients, and instructions
 function Recipe({ recipe, ingredients, instructions }) {
+  const [showRecipe, setShowRecipe] = useState(false);
   return (
     // Changed to a div and added a className for styling purposes
-    <div className="recipe-card">
-      <h2>{recipe}</h2>
-      <h3>Ingredients:</h3>
-      <ul>
-        {/* Adding a map to iterate over the ingredients object so that they can be placed on separate lines */}
-        {ingredients.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+    <Card className="recipe-card" sx={{ maxWidth: 500 }}>
+      <CardHeader title={recipe} />
+      <CardContent>
+        <Typography variant="h6" component="h2">Ingredients:</Typography>
+        <ul>
+          {/* Adding a map to iterate over the ingredients object so that they can be placed on separate lines */}
+          {ingredients.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+        
+        <Button onClick={()=>setShowRecipe(!showRecipe)}> Show Instructions</Button>
+        {showRecipe && (
+          <div>
 
-      <h3>Instructions:</h3>
-      <ol>
-        {/* Adding a map to iterate over the instructions object so that they can be placed on separate lines */}
-        {instructions.map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
-      </ol>
-    </div>
+            <Typography variant="h6" component="h2">Instructions:</Typography>
+            <ol>
+              {/* Adding a map to iterate over the instructions object so that they can be placed on separate lines */}
+              {instructions.map((step, index) => (
+                <li key={index}>{step}</li>
+              ))}
+            </ol>
+          </div>
+        )}
+      </CardContent>
+
+    </Card>
   );
 }
 
--- src/components/RecipeContainer.jsx
+++ src/components/RecipeContainer.jsx
@@ -1,10 +1,11 @@
 // Importing child/presentational component to Higher-Order Component
+import { Grid } from "@mui/material";
 import Recipe from "./Recipe.jsx"
 
 //Higher-Order Component - Passing in information from App to be used as props by Recipe
 function RecipeContainer({ recipes }) {
     return(
-        <div>
+        <Grid container alignItems="center" justifyContent="space-between" spacing={2}>
             {/* Using a map to loop through the recipes and rendering a Recipe component for each */}
             {/* Created a key since an id was not provided with the dataset */}
             {recipes.map(( recipe, index ) => (
@@ -15,7 +16,7 @@ function RecipeContainer({ recipes }) {
                 />
                 ))
             }
-        </div>
+        </Grid>
     );
 }
 
